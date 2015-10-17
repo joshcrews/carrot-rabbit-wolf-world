d418d5247cd6f0aaa63cac078642e8498ef070d6
Josh Crews <josh@joshcrews.com>
2015-10-16 20:28:01 -0500

    squash test for rabbit ages and dies

--- test/rabbit_test.exs
+++ test/rabbit_test.exs
@@ -29,4 +29,10 @@ defmodule RabbitTest do
     assert Rabbit.make_babies(rabbit) == %Rabbit{carrots_in_belly: 0, current_coordinates: %{x: 0, y: 0}}
   end
 
+  test "ages and dies" do
+    rabbit = %Rabbit{days_since_last_carrots: 10, alive: true}
+    new_rabbit = Rabbit.age(rabbit) |> Rabbit.die
+    assert new_rabbit == %Rabbit{alive: false, days_since_last_carrots: 11}
+  end
+
 end
